c65caab3d53581fd1975a8b20f5960d1e0145239
João Vitor Pereira <251joaovitor@gmail.com>
2021-03-15 12:14:58 +0000

    (15/03) Término de selection sort e início de merge sort

--- ativ01-revisao-javascript (MINHA TENTATIVA).js
+++ ativ01-revisao-javascript (MINHA TENTATIVA).js
@@ -31,11 +31,85 @@ estadosNe.push(estado1)
       ao vetor estadosNe usando push().
 */
 
+let estado2 = {
+   nome: 'Alagoas',
+   sigla: 'AL',
+   area: 27848.14,
+   populacao: 3322820
+}
+
+estadosNe.push(estado2)
+
+let estado3 = {
+   nome: 'Rio Grande do Norte',
+   sigla: 'RN',
+   area: 52811.05,
+   populacao: 3479010
+}
+
+estadosNe.push(estado3)
+
+let estado4 = {
+   nome: 'Paraíba',
+   sigla: 'PB',
+   area: 56469.78,
+   populacao: 3996496
+}
+
+estadosNe.push(estado4)
+
+let estado5 = {
+   nome: 'Pernambuco',
+   sigla: 'PE',
+   area: 98148.32,
+   populacao: 9496294
+}
+
+estadosNe.push(estado5)
+
+let estado6 = {
+   nome: 'Ceará',
+   sigla: 'CE',
+   area: 148920.47,
+   populacao: 9075649
+}
+
+estadosNe.push(estado6)
+
+let estado7 = {
+   nome: 'Piauí',
+   sigla: 'PI',
+   area: 251577.74,
+   populacao: 3264531
+}
+
+estadosNe.push(estado7)
+
+let estado8 = {
+   nome: 'Maranhão',
+   sigla: 'MA',
+   area: 331937.45,
+   populacao: 7035055
+}
+
+estadosNe.push(estado8)
+
+let estado9 = {
+   nome: 'Bahia',
+   sigla: 'BA',
+   area: 564733.18,
+   populacao: 14812617
+}
+
+estadosNe.push(estado9)
+
 /*
    2) Escreva uma arrow function que receba dois parâmetros, área e população, e
       calcule a densidade demográfica retornando o resultado da divisão da
       segunda pela primeira. 
 */
+
+let DensiDemo = (area, populacao) => populacao / area
 
 /*
    3) Percorra o vetor estadosNe usando um for tradicional. Para cada estado,
@@ -47,13 +121,33 @@ estadosNe.push(estado1)
 
 */
 
+for(let i = 0; i < estadosNe.length; i++) {
+    estadosNe[i]['densidade demografica'] = DensiDemo(estadosNe[i].area, estadosNe[i].populacao).toFixed(2)
+    delete estadosNe[i].sigla
+    console.log(estadosNe[i])
+}
+
 /* 4) Escreva uma arrow function que receba um objeto. Na função, use for..in
       para extrair as propriedades e seus valores e exibi-los com console.log().
 
 */
 
+// NÃO CONSEGUI COMPREENDER COMO FAZER COM QUE A ARROW FUNCTION RECEBA UM OBJETO
+
+let extrair = estados => {
+    for(let prop in estadosNe)
+    console.log(prop)
+}
+
 /* 5) Percorra o vetor estadosNe usando for..of. Para cada objeto no vetor,
       invoque a função escrita em 4) para exibi-lo.
+*/
+
+// NÃO ENTENDI DE QUE FORMA DEVE SER CHAMADA ESSA FUNÇÃO
+
+for(let x of estadosNe) {
+    extrair(x)
+}
 
 /*
    6)
@@ -67,3 +161,16 @@ estadosNe.push(estado1)
          já existentes, e assim por diante.
 
 */
+
+let estados = []
+
+estados.push(estado1.nome)
+estados.unshift(estado2.nome)
+estados.splice(1, 0, estado3.nome)
+estados.splice(1, 0, estado4.nome)
+estados.splice(2, 0, estado5.nome)
+estados.splice(1, 0, estado6.nome)
+estados.splice(4, 0, estado7.nome)
+estados.splice(2, 0, estado8.nome)
+estados.splice(1, 0, estado9.nome)
+console.log(estados)
